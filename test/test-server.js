009a77064fafb0894d4754d1695ead2145dc4caf
Luis Vega <luis@port9.com>
2017-08-17 13:38:34 -0700

    second

---
+++ test/test-server.js
@@ -0,0 +1,19 @@
+var chai = require('chai')
+var chaiHttp = require('chai-http')
+var server = require('../app.js')
+
+var should = chai.should()
+chai.use(chaiHttp)
+
+describe('it lives', function(){
+  it('should say hello world', function(done) {
+    chai.request(server)
+      .get('/')
+      .end(function(err, res) {
+        res.should.have.status(200)
+        res.body.should.have.property('message')
+        res.body.message.should.equal("hello world")
+        done()
+      })
+  })
+})
