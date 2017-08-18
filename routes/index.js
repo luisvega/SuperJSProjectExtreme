abc1e8900f2efd31bd35fd1cc4e6d0492c802293
Luis Vega <luis@port9.com>
2017-08-18 12:15:53 -0700

    added dummy route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,7 @@ router.get('/', function(req, res, next) {
   res.status(200).json({message: "hello world"})
 });
 
+router.get('/epoch', function(req, res, next) {
+  res.status(200).json({ message: Date.now() })
+})
 module.exports = router;
